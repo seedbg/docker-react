8ba36cf2d6eec5ea9f605f4087ef29e89d9b05ee
seedbg <junk12@abv.bg>
2020-05-15 16:31:37 +0300

    slight change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React is the best yeahh
+          Obicham te kote 
               
         </p>
         <a
